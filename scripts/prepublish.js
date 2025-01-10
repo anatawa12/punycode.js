9eaca2369131905cf7998b4d19a4e6969f54bfa6
anatawa12 <anatawa12@icloud.com>
2025-01-10 10:06:31 +0900

    add esmodule support

--- scripts/prepublish.js
+++ scripts/prepublish.js
@@ -15,3 +15,4 @@ if (!regex.test(sourceContents)) {
 const outputContents = sourceContents.replace(regex, output);
 
 fs.writeFileSync(path.resolve(__dirname, '../punycode.es6.js'), outputContents);
+fs.writeFileSync(path.resolve(__dirname, '../punycode.es6.mjs'), outputContents);
